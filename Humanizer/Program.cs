c5116982ef301286a97b69a4c0b8d4848015da09
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-06 02:52:33 -0300

    Humanizer

--- Humanizer/Program.cs
+++ Humanizer/Program.cs
@@ -9,79 +9,91 @@ WriteLine(".:: Humanizer Samples ::.");
 // Configurator.DateTimeHumanizeStrategy = new PrecisionDateTimeHumanizeStrategy(precision: .75);
 // Configurator.DateTimeOffsetHumanizeStrategy = new PrecisionDateTimeOffsetHumanizeStrategy(precision: .75); // configure when humanizing DateTimeOffset
 
-WriteLine("PascalCaseInputStringIsTurnedIntoSentence".Humanize());
-WriteLine("Underscored_input_string_is_turned_into_sentence".Humanize());
-WriteLine("Underscored_input_String_is_turned_INTO_sentence".Humanize());
-WriteLine("HUMANIZER".Transform(To.LowerCase, To.TitleCase));
-WriteLine("Lon".Truncate(6));
-WriteLine("Long text to truncate".Truncate(6, ""));
-WriteLine("Long text to truncate".Truncate(6));
-WriteLine("Long text to truncate".Truncate(6, "---"));
-WriteLine("Long text to truncate".Truncate(6, "..."));
-WriteLine(DateTime.UtcNow.AddDays(-1).Humanize());
-WriteLine(DateTime.UtcNow.AddHours(-1).Humanize());
-WriteLine(DateTime.UtcNow.Humanize());
-WriteLine(DateTime.UtcNow.AddMinutes(10).Humanize());
-WriteLine(DateTime.UtcNow.AddMinutes(-10).Humanize());
-WriteLine(DateTime.UtcNow.AddMinutes(60).Humanize());
+WriteLine($"string.Humanize: " + "PascalCaseInputStringIsTurnedIntoSentence".Humanize());
+WriteLine($"string.Humanize: " + "Underscored_input_string_is_turned_into_sentence".Humanize());
+WriteLine($"string.Humanize: " + "Underscored_input_String_is_turned_INTO_sentence".Humanize());
+WriteLine();
 
-WriteLine(DateTime.UtcNow.AddSeconds(45).Humanize());
-WriteLine(DateTime.UtcNow.AddSeconds(-45).Humanize());
-WriteLine(DateTime.UtcNow.AddSeconds(46).Humanize());
-WriteLine(DateTime.UtcNow.AddSeconds(-46).Humanize());
-WriteLine(new DateTime(2021, 06, 01).Humanize());
-WriteLine(new DateTime(2021, 02, 01).Humanize());
-WriteLine(DateTime.UtcNow.ToOrdinalWords());
+WriteLine($"Truncate: " + "Lon".Truncate(6));
+WriteLine($"Truncate: " + "Long text to truncate".Truncate(6, ""));
+WriteLine($"Truncate: " + "Long text to truncate".Truncate(6));
+WriteLine($"Truncate: " + "Long text to truncate".Truncate(6, "---"));
+WriteLine($"Truncate: " + "Long text to truncate".Truncate(6, "..."));
+WriteLine();
 
-WriteLine("homem".Pluralize());
-WriteLine("homens".Pluralize());
-WriteLine("mulher".Pluralize());
-WriteLine("colher".Pluralize());
-WriteLine("carro".Pluralize());
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddDays(-1).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddHours(-1).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddMinutes(10).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddMinutes(-10).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddMinutes(60).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddSeconds(45).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddSeconds(-45).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddSeconds(46).Humanize()}");
+WriteLine($"DateTime.Humanize: {DateTime.UtcNow.AddSeconds(-46).Humanize()}");
+WriteLine($"DateTime.Humanize: {new DateTime(2021, 06, 01).Humanize()}");
+WriteLine($"DateTime.Humanize: {new DateTime(2021, 02, 01).Humanize()}");
+WriteLine();
 
-WriteLine("carro".ToQuantity(1));
-WriteLine("carro".ToQuantity(2));
+WriteLine($"Pluralize: " + "homem".Pluralize());
+WriteLine($"Pluralize: " + "homens".Pluralize());
+WriteLine($"Pluralize: " + "mulher".Pluralize());
+WriteLine($"Pluralize: " + "colher".Pluralize());
+WriteLine($"Pluralize: " + "carro".Pluralize());
+
+WriteLine($"ToQuantity: " + "carro".ToQuantity(1));
+WriteLine($"ToQuantity: " + "carro".ToQuantity(2));
+
 WriteLine($"Ordinalize: {1.Ordinalize()}");
 WriteLine($"Ordinalize: {2.Ordinalize()}");
 WriteLine($"Ordinalize: {3.Ordinalize()}");
 WriteLine($"Ordinalize: {4.Ordinalize()}");
+WriteLine();
+
 WriteLine($"ToOrdinalWords: {1.ToOrdinalWords()}");
 WriteLine($"ToOrdinalWords: {2.ToOrdinalWords()}");
 WriteLine($"ToOrdinalWords: {3.ToOrdinalWords()}");
 WriteLine($"ToOrdinalWords: {4.ToOrdinalWords()}");
+WriteLine();
 
-WriteLine("fáBio Monteiro_naspolini".Titleize());
-WriteLine("fáBio Monteiro_naspolini".Pascalize());
-WriteLine("fáBio Monteiro_naspolini".Camelize());
-WriteLine("fáBio Monteiro_naspolini".Dasherize());
-WriteLine("fáBio Monteiro_naspolini".Hyphenate());
-WriteLine("fáBio Monteiro_naspolini".Kebaberize());
+WriteLine($"Transform   : " + "HUMANIZER".Transform(To.LowerCase, To.TitleCase));
+WriteLine($"Titleize    : " + "primeira Segunda_terceira".Titleize());
+WriteLine($"Pascalize   : " + "primeira Segunda_terceira".Pascalize());
+WriteLine($"Camelize    : " + "primeira Segunda_terceira".Camelize());
+WriteLine($"Dasherize   : " + "primeira Segunda_terceira".Dasherize());
+WriteLine($"Hyphenate   : " + "primeira Segunda_terceira".Hyphenate());
+WriteLine($"Kebaberize  : " + "primeira Segunda_terceira".Kebaberize());
+WriteLine();
 
-WriteLine(1.ToWords());
-WriteLine(2.ToWords());
-WriteLine(225.ToWords());
-WriteLine(2252.ToWords());
-WriteLine(2252.ToWords(GrammaticalGender.Feminine));
+WriteLine($"ToWords: {1.ToWords()}");
+WriteLine($"ToWords: {2.ToWords()}");
+WriteLine($"ToWords: {225.ToWords()}");
+WriteLine($"ToWords: {2252.ToWords()}");
+WriteLine($"ToWords: {2252.ToWords(GrammaticalGender.Feminine)}");
+WriteLine();
 
-WriteLine(1d.ToMetric());
-WriteLine(1230d.ToMetric());
-WriteLine(0.1.ToMetric());
+WriteLine($"ToMetric: {1d.ToMetric()}");
+WriteLine($"ToMetric: {1230d.ToMetric()}");
+WriteLine($"ToMetric: {0.1.ToMetric()}");
+WriteLine();
 
 var fileSize = 50;
 var fileSize2 = 1200;
-WriteLine(fileSize.Bytes().Humanize());
-WriteLine(fileSize.Kilobytes().Humanize());
-WriteLine(fileSize.Megabytes().Humanize());
-WriteLine(fileSize.Gigabytes().Humanize());
+WriteLine($"fileSize: {fileSize.Bytes().Humanize()}");
+WriteLine($"fileSize: {fileSize.Kilobytes().Humanize()}");
+WriteLine($"fileSize: {fileSize.Megabytes().Humanize()}");
+WriteLine($"fileSize: {fileSize.Gigabytes().Humanize()}");
 
-WriteLine(fileSize2.Bytes().Humanize());
-WriteLine(fileSize2.Bytes().Humanize("KB"));
-WriteLine(fileSize2.Bytes().Humanize("MB"));
-WriteLine(fileSize2.Kilobytes().Humanize());
-WriteLine(fileSize2.Megabytes().Humanize());
-WriteLine(fileSize2.Gigabytes().Humanize());
+WriteLine($"fileSize2: {fileSize2.Bytes().Humanize()}");
+WriteLine($"fileSize2: {fileSize2.Bytes().Humanize("KB")}");
+WriteLine($"fileSize2: {fileSize2.Bytes().Humanize("MB")}");
+WriteLine($"fileSize2: {fileSize2.Kilobytes().Humanize()}");
+WriteLine($"fileSize2: {fileSize2.Megabytes().Humanize()}");
+WriteLine($"fileSize2: {fileSize2.Gigabytes().Humanize()}");
+WriteLine();
 
-WriteLine(0d.ToHeadingArrow());
-WriteLine(90d.ToHeadingArrow());
-WriteLine(180d.ToHeadingArrow());
-WriteLine(270d.ToHeadingArrow());
+WriteLine($"ToHeadingArrow: {0d.ToHeadingArrow()}");
+WriteLine($"ToHeadingArrow: {90d.ToHeadingArrow()}");
+WriteLine($"ToHeadingArrow: {180d.ToHeadingArrow()}");
+WriteLine($"ToHeadingArrow: {270d.ToHeadingArrow()}");
+WriteLine();
