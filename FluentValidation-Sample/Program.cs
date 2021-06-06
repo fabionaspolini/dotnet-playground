342c403342459fbd5b8e06b3d140e564935d9f45
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-05 23:31:49 -0300

    Atualizando

---
+++ FluentValidation-Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentValidation_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
