b8cec5887ea3b60fe73840f9d9efeba1e388baf5
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-13 18:53:41 -0300

    Mustache

---
+++ Mustache/Program.cs
@@ -0,0 +1,29 @@
+﻿using Stubble.Core.Builders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static System.Console;
+
+WriteLine(".:: Mustache Samples ::.");
+var dadosRelatorio = new Relatorio
+{
+    Titulo = "Detalhes do cliente",
+    Nome = "Fulano de tal",
+    DataNascimento = new DateTime(1989, 01, 01),
+    Contatos = new() { "Pedro", "Paulo", "José" }
+};
+var template = await File.ReadAllTextAsync(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../relatorio.mustache"), Encoding.UTF8);
+var stubble = new StubbleBuilder().Build();
+var result = await stubble.RenderAsync(template, dadosRelatorio);
+WriteLine(result);
+
+class Relatorio
+{
+    public string Titulo { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public bool MaiorDeIdade => DateTime.Now - DataNascimento >= TimeSpan.FromDays(18 * 365);
+    public List<string> Contatos { get; set; }
+    public bool PossuiContatos => Contatos != null && Contatos.Count > 0;
+}
