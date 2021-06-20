957263dca95dd87993d1c83f4a4e29c29d904ce5
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-20 00:55:39 -0300

    CacheManager

--- CacheManager/Program.cs
+++ CacheManager/Program.cs
@@ -30,27 +30,34 @@ var config = new ConfigurationBuilder()
 // Objeto gestor de cache para classe "Pessoa"
 var pessoaCache = new BaseCacheManager<Pessoa>(config);
 
+// Na primeira execução irá criar a Pessoa. Se executar o demo novamente antes de 2 minutos, obterá do redis e sincronizará para o Handler de memória da aplicação.
 logger.LogInformation("Get 1" + new string('-', 100));
-var pessoaGet1 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger)); // Irá criar a Pessoa. Ao executar novamente o demo antes de 2 minutos, obterá do redis.
+var pessoaGet1 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
+// Caso estiver executando pela primeira vez o demo, neste momento que o cache do redis será sincronizado para a memória da RAM. Caso já existi-se o cache na execuçaõ do Get 1, na linha acima estaria armazenando também na memória.
 logger.LogInformation("Get 2" + new string('-', 100));
-var pessoaGet2 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger)); // Caso estiver executando pela primeira vez o demo, neste momento que o cache do redis será sincronizado para a memória da RAM. Caso já existi-se o cache na execuçaõ do Get 1, na linha acima estaria armazenando também na memória
+var pessoaGet2 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
 logger.LogInformation("Get 3" + new string('-', 100));
 var pessoaGet3 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
 
+// Aqui o cache continua a obter da memória
 logger.LogInformation("Get 4" + new string('-', 100));
 logger.LogInformation("Delay 3 segundos...");
 await Task.Delay(3000);
-var pessoaGet4 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger)); // Aqui o cache continuar na memória
+var pessoaGet4 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
 
+// Aqui o cache continua a obter da memória
 logger.LogInformation("Get 5" + new string('-', 100));
 logger.LogInformation("Delay 3 segundos...");
 await Task.Delay(3000);
-var pessoaGet5 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger)); // Aqui o cache continuar na memória
+var pessoaGet5 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
 
+ // Aqui o cache já foi removido da memória porque está com a configuração "Sliding" em 5 segundos, mas será obtido do Redis e resincronizado para memória
 logger.LogInformation("Get 6" + new string('-', 100));
 logger.LogInformation("Delay 6 segundos...");
 await Task.Delay(6000);
-var pessoaGet6 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger)); // Aqui o cache já foi removido da memória porque está com a configuração "Slidind" em 5 segundos, mas será obtido do Redis e resincronizado para memória
+var pessoaGet6 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
 
 logger.LogInformation(new string('=', 100));
 logger.LogInformation($"Pessoa: {pessoaGet1.Nome} | {pessoaGet1.DataCadastro}");
@@ -75,6 +82,17 @@ logger.LogInformation("KeyB is " + cache.Get("keyB")); // Deve ser 42
 
 await Task.Delay(1000); // Para dar tempo do ILogger descarregar tudo no console
 
+// Alternativa para configurar e instânciar cache inline
+var cacheConfiguradoInline = CacheFactory.Build<Pessoa>(settings => settings
+    .WithUpdateMode(CacheUpdateMode.Up)
+    .WithJsonSerializer()
+    .WithRedisConfiguration("redis", "localhost:6379,ssl=false")
+    .WithMicrosoftLogging(loggerFactory)
+    .WithSystemRuntimeCacheHandle("memory")
+        .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromSeconds(5))
+    .And.WithRedisCacheHandle("redis")
+        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(120)));
+
 public class Pessoa
 {
     public int Id { get; set; }
@@ -96,14 +114,3 @@ public static class PessoaFactory
         };
     }
 }
-
-// Alternativa para configurar e instânciar cache inline
-/*var pessoaCache = CacheFactory.Build<Pessoa>(settings => settings
-    .WithUpdateMode(CacheUpdateMode.Up)
-    .WithJsonSerializer()
-    .WithRedisConfiguration("redis", "localhost:6379,ssl=false")
-    .WithMicrosoftLogging(loggerFactory)
-    .WithSystemRuntimeCacheHandle("memory")
-        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(60))
-    .And.WithRedisCacheHandle("redis")
-        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(120)));*/
