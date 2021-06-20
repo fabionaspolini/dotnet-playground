3c8d7bc8654e3aca68e37ac1bdb3048d3d02fb43
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-20 00:01:18 -0300

    CacheManager

--- CacheManager/Program.cs
+++ CacheManager/Program.cs
@@ -1,22 +1,60 @@
 ﻿using System;
+using System.Threading.Tasks;
 using CacheManager.Core;
-using static System.Console;
+using Microsoft.Extensions.Logging;
 
-WriteLine(".:: Cache Manager Samples ::.");
+var loggerFactory = LoggerFactory.Create(builder => builder
+    .AddFilter("CacheManager", LogLevel.Trace)
+    .AddFilter("CacheReflectionHelper", LogLevel.Trace)
+    .AddSimpleConsole(opts =>
+    {
+        opts.SingleLine = true;
+        opts.IncludeScopes = true;
+        opts.TimestampFormat = "dd/MM/yyyy HH:mm:ss.fff ";
+    }));
+var logger = loggerFactory.CreateLogger("CachaSample");
+logger.LogInformation(".:: Cache Manager Samples ::.");
 
-var cache = CacheFactory.Build<Pessoa>("getStartedCache",
-    settings => settings
-        .WithUpdateMode(CacheUpdateMode.Up)
-            .WithSystemRuntimeCacheHandle("handleName")
-            .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(60))
-        .And.WithRedisConfiguration("redisLocal", "localhost:6379,ssl=false")
-            .WithRedisCacheHandle("redisLocal")
-            .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(120))
-        .And.WithJsonSerializer());
+// Criando configuração de log reaproveitável para N objetos de cache
+var config = new ConfigurationBuilder()
+    .WithUpdateMode(CacheUpdateMode.Up)
+    .WithJsonSerializer()
+    .WithRedisConfiguration("redis", "localhost:6379,ssl=false")
+    .WithMicrosoftLogging(loggerFactory)
+    .WithSystemRuntimeCacheHandle("memory")
+        .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromSeconds(5)) // Sliding => Expirar cache após 5 segundos sem acesso
+    .And.WithRedisCacheHandle("redis")
+        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(120)) // Absolute => Expirar cache 120 segundos após criação
+    .Build();
 
-var pessoa = cache.GetOrAdd("pessoa", key => PessoaFactory.Create());
-WriteLine($"Pessoa: {pessoa.Nome} | {pessoa.DataCadastro}");
-WriteLine();
+// Objeto gestor de cache para classe "Pessoa"
+var pessoaCache = new BaseCacheManager<Pessoa>(config);
+
+logger.LogInformation("Get 1" + new string('-', 100));
+var pessoaGet1 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+logger.LogInformation("Get 2" + new string('-', 100));
+var pessoaGet2 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+logger.LogInformation("Get 3" + new string('-', 100));
+var pessoaGet3 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
+logger.LogInformation("Get 4" + new string('-', 100));
+logger.LogInformation("Delay 3 segundos...");
+await Task.Delay(3000);
+var pessoaGet4 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
+logger.LogInformation("Get 5" + new string('-', 100));
+logger.LogInformation("Delay 3 segundos...");
+await Task.Delay(3000);
+var pessoaGet5 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
+logger.LogInformation("Get 6" + new string('-', 100));
+logger.LogInformation("Delay 6 segundos...");
+await Task.Delay(6000);
+var pessoaGet6 = pessoaCache.GetOrAdd("pessoa", key => PessoaFactory.Create(logger));
+
+logger.LogInformation(new string('=', 100));
+logger.LogInformation($"Pessoa: {pessoaGet1.Nome} | {pessoaGet1.DataCadastro}");
+await Task.Delay(1000); // Para dar tempo do ILogger descarregar tudo no console
 
 /*cache.Add("keyA", "valueA");
 cache.Put("keyB", 23);
@@ -59,9 +97,9 @@ public class Pessoa
 
 public static class PessoaFactory
 {
-    public static Pessoa Create()
+    public static Pessoa Create(ILogger logger)
     {
-        WriteLine(">>> Gerando nova pessoa");
+        logger.LogInformation(">>> Gerando nova pessoa");
         return new Pessoa
         {
             Id = 1,
@@ -70,3 +108,14 @@ public static class PessoaFactory
         };
     }
 }
+
+// Alternativa para configurar e instância cache inline
+/*var pessoaCache = CacheFactory.Build<Pessoa>(settings => settings
+    .WithUpdateMode(CacheUpdateMode.Up)
+    .WithJsonSerializer()
+    .WithRedisConfiguration("redis", "localhost:6379,ssl=false")
+    .WithMicrosoftLogging(loggerFactory)
+    .WithSystemRuntimeCacheHandle("memory")
+        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(60))
+    .And.WithRedisCacheHandle("redis")
+        .WithExpiration(ExpirationMode.Absolute, TimeSpan.FromSeconds(120)));*/
