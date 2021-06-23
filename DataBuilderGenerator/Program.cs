8a58e55b5be7b30eac95a8e1102079d974c41714
Fábio Naspolini <fabio.naspolini@ao3tech.com>
2021-06-23 02:06:22 -0300

    Databuilder generator

---
+++ DataBuilderGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DasMulli.DataBuilderGenerator;
+using DataBuilderGenerator_Sample;
+using static System.Console;
+
+WriteLine(".:: Data Builder Generator ::.");
+
+var pessoa1 = new Person("Primeiro", "Ultimo");
+
+var pessoa2 = new PersonBuilder()
+    .WithFirstName("Primeiro")
+    .WithMiddleNames("Meio")
+    .WithLastName("Ultimo")
+    .Build();
+
+var pessoa3 = new PersonBuilder(pessoa2)
+    .WithoutMiddleNames()
+    .Build();
+
+WriteLine($"Pessoa 1: {pessoa1.GetFullName()}");
+WriteLine($"Pessoa 2: {pessoa2.GetFullName()}");
+WriteLine($"Pessoa 3: {pessoa3.GetFullName()}");
+
+namespace DataBuilderGenerator_Sample
+{
+    [GenerateDataBuilder]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string? MiddleNames { get; set; } = "Initial value";
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string GetFullName() => $"{FirstName} {MiddleNames} {LastName}";
+    }
+}
