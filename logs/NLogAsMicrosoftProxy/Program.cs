5aef5591a8f1a2f70ebd675c197ec27a5cf7a47b
Fábio Naspolini <fabionaspolini@gmail.com>
2023-03-13 14:52:59 -0300

    nlog

--- logs/NLogAsMicrosoftProxy/Program.cs
+++ logs/NLogAsMicrosoftProxy/Program.cs
@@ -7,92 +7,92 @@ using System;
 using System.Diagnostics;
 using static System.Console;
 
-namespace NLogAsMicrosoftProxy_Sample
+namespace NLogAsMicrosoftProxy_Sample;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        WriteLine(".:: NLog como proxy para Microsoft Logging ::.");
+            
+        Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+            
+        var config = new ConfigurationBuilder().Build();
+        var serviceProvider = BuildDi(config);
+            
+        using var scope = serviceProvider.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            
+        try
         {
-            WriteLine(".:: NLog como proxy para Microsoft Logging ::.");
-            var nlogger = LogManager.GetCurrentClassLogger();
+            var teste = scope.ServiceProvider.GetRequiredService<Teste>();
+
+            logger.LogInformation("Iniciando aplicação");
+
+            teste.ConverterParaInt("1", false);
+            teste.ConverterParaInt("aaaa", false);
             try
             {
-                Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+                teste.ConverterParaInt("bbbb", true);
+            }
+            catch { }
 
-                var config = new ConfigurationBuilder().Build();
-                var serviceProvider = BuildDi(config);
 
-                using var scope = serviceProvider.CreateScope();
-                var teste = scope.ServiceProvider.GetRequiredService<Teste>();
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            logger.LogDebug("Exemplo de outro log estruturado {nome} {idade}", "Exemplo", 25); // Exemplo de log estruturado montando objeto { nome = "Exemplo", idade = 25 }
+            logger.LogTrace("Exemplo Trace");
+            logger.LogDebug("Exemplo Debug");
+            logger.LogInformation("Exemplo Info");
+            logger.LogWarning("Exemplo Warn");
+            logger.LogError("Exemplo Error");
+            logger.LogCritical("Exemplo Fatal");
+            LogManager.GetCurrentClassLogger().Info("Comparativo de log acessando enviado diretamente para o NLog");
 
-                logger.LogInformation("Iniciando aplicação");
-
-                teste.ConverterParaInt("1", false);
-                teste.ConverterParaInt("aaaa", false);
+            using (logger.BeginScope("Exemplo de escopo (nome={nome}, idade={idade})", "Exemplo", 25))
+            {
                 try
                 {
-                    teste.ConverterParaInt("bbbb", true);
+
+                    logger.LogInformation("Teste");
+                    logger.LogInformation("Teste 2");
+                    using (logger.BeginScope("Exemplo de subescopo (endereco={endereco}, cidade={cidade})", "Rua exemplo", "São Paulo"))
+                    {
+                        logger.LogInformation("Teste 3");
+                        logger.LogInformation("Teste 4");
+                        throw new Exception("Erro exemplo");
+                    }
                 }
-                catch { }
-
-
-                logger.LogDebug("Exemplo de outro log estruturado {nome} {idade}.", "Exemplo", 25); // Exemplo de log estruturado montando objeto { nome = "Exemplo", idade = 25 }
-                logger.LogTrace("Exemplo Trace");
-                logger.LogDebug("Exemplo Debug");
-                logger.LogInformation("Exemplo Info");
-                logger.LogWarning("Exemplo Warn");
-                logger.LogError("Exemplo Error");
-                logger.LogCritical("Exemplo Fatal");
-                nlogger.Info("Exemplo log acessando diretamente NLog");
-
-                using (logger.BeginScope("Exemplo de escopo (nome={nome}, idade={idade})", "Exemplo", 25))
+                catch (Exception e)
                 {
-                    try
-                    {
-
-                        logger.LogInformation("Teste");
-                        logger.LogInformation("Teste 2");
-                        using (logger.BeginScope("Exemplo de subescopo (endereco={endereco}, cidade={cidade})", "Rua exemplo", "São Paulo"))
-                        {
-                            logger.LogInformation("Teste 3");
-                            logger.LogInformation("Teste 4");
-                            throw new Exception("Erro exemplo");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        logger.LogCritical(e, "Erro ao realizar ....");
-                    }
+                    logger.LogCritical(e, "Erro ao realizar ....");
                 }
             }
-            catch (Exception e)
-            {
-                nlogger.Error(e, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Aguardar flush de todos os logs para o targets
-                LogManager.Shutdown();
-            }
         }
-
-        private static IServiceProvider BuildDi(IConfiguration config)
+        catch (Exception e)
         {
-            return new ServiceCollection()
-               .AddScoped<Teste>() // Runner is the custom class
-               .AddLogging(loggingBuilder =>
-               {
-                   // configure Logging with NLog
-                   loggingBuilder.ClearProviders();
-                   loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                   loggingBuilder.AddNLog(config, new NLogProviderOptions
-                   {
-                       IncludeScopes = true
-                   });
-               })
-               .BuildServiceProvider();
+            logger.LogError(e, "Stopped program because of exception");
+            throw;
         }
+        finally
+        {
+            // Aguardar flush de todos os logs para o targets
+            LogManager.Shutdown();
+        }
+    }
+
+    private static IServiceProvider BuildDi(IConfiguration config)
+    {
+        return new ServiceCollection()
+            .AddScoped<Teste>() // Runner is the custom class
+            .AddLogging(loggingBuilder =>
+            {
+                // configure Logging with NLog
+                loggingBuilder.ClearProviders();
+                loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                loggingBuilder.AddNLog(config, new NLogProviderOptions
+                {
+                    IncludeScopes = true
+                });
+            })
+            .BuildServiceProvider();
     }
 }
