18d88e391d69327d3ff9f6d608fa344897cab735
Fábio Naspolini <fabionaspolini@gmail.com>
2023-08-03 01:23:32 -0300

    grpc

---
+++ grpc/README.md
@@ -0,0 +1,3 @@
+# gRPC
+
+- [Tutorial para criar server e client](https://learn.microsoft.com/en-us/aspnet/core/tutorials/grpc/grpc-start?view=aspnetcore-7.0&tabs=visual-studio)
---
+++ grpc/basic-grpc-client/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BasicGrpcClientPlayground;
+using Grpc.Net.Client;
+using System.Diagnostics;
+
+Console.WriteLine(".:: gRPC Playground - Basic Client ::.");
+
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+var channelOptions = new GrpcChannelOptions { HttpHandler = httpHandler };
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7130", channelOptions);
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Teste" });
+Console.WriteLine("Greeting: " + reply.Message);
+
+var watch = Stopwatch.StartNew();
+var timing = TimeSpan.FromSeconds(3);
+var count = 0;
+while (watch.Elapsed <= timing)
+{
+    await client.SayHelloAsync(new HelloRequest { Name = "Teste" });
+    count++;
+}
+watch.Stop();
+
+Console.WriteLine($"{count} requisições em {watch.Elapsed}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
