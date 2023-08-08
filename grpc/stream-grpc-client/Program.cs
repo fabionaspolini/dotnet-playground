e86725973043d3dc3757f0381a42ea4b38858b41
Fábio Naspolini <fabionaspolini@gmail.com>
2023-08-07 22:20:30 -0300

    grpc stream

---
+++ grpc/stream-grpc-client/Program.cs
@@ -0,0 +1,46 @@
+﻿using StreamGrpcClientPlayground;
+using Grpc.Net.Client;
+
+Console.WriteLine(".:: gRPC Playground - Basic Client ::.");
+
+const string Protocol = "HTTP";
+
+// Setup clients
+GrpcChannel channel;
+if (Protocol == "HTTPS")
+{
+    var httpHandler = new HttpClientHandler();
+    httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+    var channelOptions = new GrpcChannelOptions { HttpHandler = httpHandler };
+    channel = GrpcChannel.ForAddress("https://localhost:7130", channelOptions);
+}
+else
+{
+    /*var channelOptions = new GrpcChannelOptions
+    {
+        HttpHandler = new SocketsHttpHandler
+        {
+            PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+            EnableMultipleHttp2Connections = true,
+            UseCookies = false,
+            PreAuthenticate = false,
+        }
+    };
+    channel = GrpcChannel.ForAddress("http://localhost:5155", channelOptions);*/
+    channel = GrpcChannel.ForAddress("http://localhost:5155");
+}
+
+
+// Tests
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Teste" });
+Console.WriteLine("gRPC test response: " + reply.Message);
+
+Console.WriteLine($"Protocolo: {Protocol}");
+
+// gRPC benchmark
+var response = await client.SayHelloAsync(new HelloRequest { Name = "Teste" });
+Console.WriteLine($"Response: {response.Message}");
+
+Console.WriteLine("Fim");
---
+++ grpc/stream-grpc-server/Program.cs
@@ -0,0 +1,26 @@
+using StreamGrpcServerPlayground.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+// https://learn.microsoft.com/en-us/aspnet/core/grpc/aspnetcore?view=aspnetcore-7.0&tabs=visual-studio
+/*builder.WebHost.ConfigureKestrel(options =>
+{
+    //var http2 = options.Limits.Http2;
+    //http2.InitialConnectionWindowSize = 1024 * 1024 * 2; // 2 MB
+    //http2.InitialStreamWindowSize = 1024 * 1024; // 1 MB
+    //options.Listen()
+});*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
